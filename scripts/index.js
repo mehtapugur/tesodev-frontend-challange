36db59540496627e7a2495c30e8c1a51c49de87c
Mehtap Uğur <yaziliminval@hotmail.com>
2021-09-26 21:14:29 +0300

    Try to use data

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,21 @@
-<>
+const dataList = document.getElementById("dataList");
+const searchBar = document.getElementById("searchBar");
+//console.log(dataList);
+
+let dataResults = [];
+
+searchBar.addEventListener("keyup", (e) => {
+  console.log(e.target.value);
+});
+
+const loadList = async () => {
+  try {
+    const res = await fetch(".././assets/mockData.json");
+    dataResults = await res.json();
+    console.log(dataResults);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+loadList();
