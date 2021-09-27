d713f7607e983cd0d681ae6e344075bf08257409
Mehtap Uğur <yaziliminval@hotmail.com>
2021-09-27 03:30:55 +0300

    Add error feature

--- scripts/index.js
+++ scripts/index.js
@@ -48,12 +48,12 @@ searchBar.addEventListener("keyup", (e) => {
   const filteredData = dataResults.data.filter((item) => {
     //console.log(item[0]);
     return (
-      item[0].includes(searchString) ||
+      item[0].toLowerCase().includes(searchString) ||
       //item[1].includes(searchString) ||
       //item[2].includes(searchString) ||
       //item[3].includes(searchString) ||
-      item[4].includes(searchString) ||
-      item[5].includes(searchString)
+      item[4].toLowerCase().includes(searchString) ||
+      item[5].toLowerCase().includes(searchString)
     );
   });
 
@@ -74,20 +74,39 @@ const loadList = async () => {
 
 const displayFilteredData = (items) => {
   const strData = items
+    .slice(0, 3)
     .map((item) => {
       return `
       <li class="list-item">
-        <h2>${item[0]}</h2>
-        <p>${item[5]} - ${item[4]}</p>
+      <div>
+        <h2>${item[5]} - ${item[4]}</h2>
+        <p>${item[0]} - ${item[3].slice(6, 10)}</p>
+      </div>
+        <h2>Email: ${item[2]}</h2>
       </li>
     `;
       //dataList.append(li);
     })
     .join("");
 
+  dataList.innerHTML = strData;
+
+  if (items.length > 3) {
+    //console.log(items.length);
+    let liDOM = document.createElement("li");
+    liDOM.innerHTML = `<a id="showMore" href="">Show more...</a>`;
+    dataList.append(liDOM);
+  }
+
+  if (items.length == 0) {
+    searchBar.style["outline"] = "none";
+    searchBar.style["border"] = "2px solid red";
+    searchBar.style["color"] = "red";
+  }
+
   //var li = document.createElement("li");
   //dataList.append(strData);
-  dataList.innerHTML = strData;
+
   //<h2>${item.name}</h2>
 };
 
